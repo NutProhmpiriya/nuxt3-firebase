217b1dc58d2f6dce21e57b3ff2e0ab23ba545187
Nut Prohmpiriya <nut.prohmpiriya@gmail.com>
2024-06-25 14:23:57 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex h-screen">
+        <div class="m-auto">
+            <UButton>FACEBOOK</UButton>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const inputValue = ref<string>('');
+    
+</script>
+
+<style>
+
+</style>
