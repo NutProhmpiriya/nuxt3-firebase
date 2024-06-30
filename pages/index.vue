79a27c66f76989009959c978d6970b78a99a3998
Nut Prohmpiriya <nut.prohmpiriya@gmail.com>
2024-07-01 00:48:35 +0700

    fix loading btn

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="flex flex-col">
         <div class="mx-auto flex flex-col">
-            <UButton class="my-3" @click="facebookAuthWithRedirect">Facebook Redirect</UButton>
+            <UButton color="blue" :loading="buttonLoading" class="my-3"  @click="facebookAuthWithRedirect">Facebook Redirect</UButton>
 
-            <UButton class="my-3" @click="facebookAuthWithPopup">Facebook Popup</UButton>
+            <UButton color="blue" :loading="buttonLoading" class="my-3" @click="facebookAuthWithPopup">Facebook Popup</UButton>
 
-            <UButton class="my-3" @click="googleAuthWithRedirect">Google Redirect</UButton>
+            <UButton :loading="buttonLoading" class="my-3" @click="googleAuthWithRedirect">Google Redirect</UButton>
 
-            <UButton class="my-3" @click="googleAuthWithPopup">Google Popup</UButton>
+            <UButton :loading="buttonLoading" class="my-3" @click="googleAuthWithPopup">Google Popup</UButton>
 
-            <UButton class="my-3" @click="logout">Logout</UButton>
+            <UButton color="red" :loading="buttonLoading" class="my-3" @click="logout">Logout</UButton>
 
         </div>
         <div class="p-5">
@@ -20,81 +20,109 @@
 </template>
 
 <script setup lang="ts">
-import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, FacebookAuthProvider, getRedirectResult, signOut } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, FacebookAuthProvider, getRedirectResult, signOut, type User } from 'firebase/auth'
 
 const user = ref(null)
 const errorMessage = ref(null)
-
+const buttonLoading = ref(false)
+const resultLogout = ref('')
 
 const nuxtApp = useNuxtApp()
 
-onMounted(async() => {
-    console.log('onMounted')
-    const authRedirectResult = await getRedirectResult(nuxtApp.$auth)
-    console.log({ authRedirectResult })
+onMounted(async () => {
+    try {
+        console.log('onMounted')
+        const authRedirectResult = await getRedirectResult(nuxtApp.$auth)
+        if (authRedirectResult?.user) {
+            console.log({authRedirectResult})
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 const facebookAuthWithRedirect = () => {
-    console.log('facebookAuthWithRedirect');
+    buttonLoading.value = true
     const provider = new FacebookAuthProvider()
     signInWithRedirect(nuxtApp.$auth, provider)
         .then((result: any) => {
             console.log(result)
             user.value = result.user
-        }).catch((error: any) => {
+        })
+        .catch((error: any) => {
             console.log(error.code)
             errorMessage.value = error.code
+        })
+        .finally(() => {
+            buttonLoading.value = false
         });
 }
 
 const facebookAuthWithPopup = () => {
-    console.log('facebookAuthWithPopup');
+    buttonLoading.value = true
     const provider = new FacebookAuthProvider()
     signInWithPopup(nuxtApp.$auth, provider)
         .then((result: any) => {
             console.log(result)
             user.value = result.user
-        }).catch((error: any) => {
+        })
+        .catch((error: any) => {
             console.log(error.code)
             errorMessage.value = error.code
+        })
+        .finally(() => {
+            buttonLoading.value = false
         });
 }
 
 const googleAuthWithRedirect = () => {
+    buttonLoading.value = true
     const provider = new GoogleAuthProvider()
     signInWithRedirect(nuxtApp.$auth, provider)
         .then((result: any) => {
             console.log(result)
             user.value = result.user
-        }).catch((error: any) => {
+        })
+        .catch((error: any) => {
             console.log(error.code)
-            errorMessage.value = error
+            errorMessage.value = error.code
+        })
+        .finally(() => {
+            buttonLoading.value = false
         });
 
 }
 
 const googleAuthWithPopup = () => {
-    console.log('googleAuthWithPopup');
+    buttonLoading.value = true
     const provider = new GoogleAuthProvider()
     signInWithPopup(nuxtApp.$auth, provider)
         .then((result: any) => {
             console.log(result)
             user.value = result.user
-        }).catch((error: any) => {
+        })
+        .catch((error: any) => {
             console.log(error.code)
-            errorMessage.value = error.message
+            errorMessage.value = error.code
+        })
+        .finally(() => {
+            buttonLoading.value = false
         });
 }
 
 const logout = () => {
-    console.log('logout');
+    buttonLoading.value = true
     signOut(nuxtApp.$auth)
         .then(() => {
             console.log('logout success')
             user.value = null
-        }).catch((error: any) => {
+        })
+        .catch((error: any) => {
             console.log(error.code)
             errorMessage.value = error.code
+        })
+        .finally(() => {
+            buttonLoading.value = false
         });
 }
     
